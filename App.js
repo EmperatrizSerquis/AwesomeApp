930c89638f3edc23cddaee40316d10c877baad6b
EmperatrizSerquis <59320647+EmperatrizSerquis@users.noreply.github.com>
2023-08-17 12:53:09 -0300

    Adding Styles

--- App.js
+++ App.js
@@ -4,7 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>WELCOME!</Text>
+      
+      <Text style={styles.title}>FRIENDS BUILDER APP!</Text>
+      <Text>APP to remember calling friends or clients depending on the category every x amount of time !</Text>
+      <Text>NOT DECIDED YET!</Text>
+      <Text style={styles.title}>FIND PROPERTIES APP!</Text>
+      <Text>APP to find selling properties!</Text>
+      <Text style={styles.title}>Personalized quotes for services or Chatbot!</Text>
+      
+
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +24,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
+  title: {
+    color: '#7858F2',
+    fontSize: '30px',
+    fontWeight: 'bold'
+  }
 });
